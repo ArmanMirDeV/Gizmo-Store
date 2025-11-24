01c56c148b69ef0d1db1515c7c732515ca69eb37
Arman Mir <mirarman8583@gmail.com>
2025-11-24 13:38:57 +0600

    header, footer layout created

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,15 @@
+import Container from '@/components/Container'
 import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div className='p-10 bg-shop-light-pink'>
+    <Container className=' bg-shop-light-pink'>
       <h2>Home
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur expedita sed ex optio molestias veniam voluptatibus qui illo nam voluptatem atque, in quod molestiae a culpa asperiores adipisci eveniet enim dignissimos at quibusdam sit animi nisi vel. Delectus omnis, vel impedit nisi non laboriosam dolorum expedita praesentium, perspiciatis porro qui!</p>
       </h2>
       <Button  size="lg" >Check</Button>
-    </div>
+    </Container>
   )
 }
 
---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "./Container";
+
+const Header = () => {
+  return (
+    <header>
+      <Container>header</Container>
+    </header>
+  );
+};
+
+export default Header;
