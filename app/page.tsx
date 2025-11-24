fe24e58d60299708631b219388102d2b03447f0c
Arman Mir <mirarman8583@gmail.com>
2025-11-24 12:33:05 +0600

    shadcn installed and button added

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
       <h2>Home
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur expedita sed ex optio molestias veniam voluptatibus qui illo nam voluptatem atque, in quod molestiae a culpa asperiores adipisci eveniet enim dignissimos at quibusdam sit animi nisi vel. Delectus omnis, vel impedit nisi non laboriosam dolorum expedita praesentium, perspiciatis porro qui!</p>
       </h2>
+      <Button  size="lg" >Check</Button>
     </div>
   )
 }
