688eb4ebf8c07baa74bf7a0270474dfef073efc4
Arman Mir <mirarman8583@gmail.com>
2025-11-24 20:26:57 +0600

    Assets added and HomeBanner Created

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,13 @@
 import Container from '@/components/Container'
-import { Button } from '@/components/ui/button'
+import HomeBanner from '@/components/HomeBanner'
 import React from 'react'
 
 const Home = () => {
   return (
-    <Container className=' bg-shop-light-pink'>
-      <h2>Home
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur expedita sed ex optio molestias veniam voluptatibus qui illo nam voluptatem atque, in quod molestiae a culpa asperiores adipisci eveniet enim dignissimos at quibusdam sit animi nisi vel. Delectus omnis, vel impedit nisi non laboriosam dolorum expedita praesentium, perspiciatis porro qui!</p>
-      </h2>
-      <Button  size="lg" >Check</Button>
+    <Container className="bg-shop-light-pink">
+      <HomeBanner />
+
+
     </Container>
   )
 }
