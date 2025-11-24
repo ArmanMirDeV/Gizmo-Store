5cd471ccf72453999d8108217d38038636bb90bb
Arman Mir <mirarman8583@gmail.com>
2025-11-24 15:43:33 +0600

    Menu bars implemented on sidebar

--- components/SideMenu.tsx
+++ components/SideMenu.tsx
@@ -1,15 +1,20 @@
 import React, { FC } from "react";
 import Logo from "./Logo";
 import { X } from "lucide-react";
+import { headerData } from "@/constants/data";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 interface SidebarProps {
   isOpen: boolean;
   onClose: () => void;
 }
 
 const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
+  const pathname = usePathname();
+
   return (
     <div
-      className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/80 shadow-xl 
+      className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white/70 shadow-xl 
       ${isOpen ? "translate-x-0" : "-translate-x-full"} 
         hoverEffect
               `}
@@ -18,11 +23,26 @@ const SideMenu: FC<SidebarProps> = ({ isOpen, onClose }) => {
         <div className="flex items-center justify-between gap-5  ">
           <Logo className="text-white  " spanDesign="group-hover:text-white " />
 
-          <button onClick={onClose} className="hover:text-shop_light_green hoverEffect  " >
+          <button
+            onClick={onClose}
+            className="hover:text-shop_light_green hoverEffect  "
+          >
             <X />
           </button>
         </div>
-        <div className=""></div>
+        <div className="flex flex-col space-y-3.5 font-semibold tracking-wide ">
+          {headerData?.map((item) => (
+            <Link
+              className={`hover:text-shop_light_green hoverEffect ${
+                pathname === item?.href && "text-white"
+              } `}
+              key={item?.title}
+              href={item.href}
+            >
+              {item?.title}
+            </Link>
+          ))}
+        </div>
       </div>
     </div>
   );
